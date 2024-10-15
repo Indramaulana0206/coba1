54893089ca17e3cac683d8e82f17895e47430962
Indramaulana0206 <indralav4@gmail.com>
2024-10-15 22:10:25 +0700

    third

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+//route resource for products
+Route::resource('/products', \app\Http\Controllers\ProductController::class);;
