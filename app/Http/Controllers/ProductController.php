e3a4cf08c585c7323869651842f1ca3c5d848e28
Indramaulana0206 <indralav4@gmail.com>
2024-10-15 20:00:46 +0700

    second

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Product;
 use Illuminate\Http\Request;
+//import return type View
 use Illuminate\View\View;
 
 class ProductController extends Controller
